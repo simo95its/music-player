1997a141848a3187a71418718f2b07f72b402871
Elena <elena.dinunzio@edu.itspiemonte.it>
2017-11-05 14:26:03 +0100

    modifiche alla gui

--- public/main.js
+++ public/main.js
@@ -8,7 +8,6 @@
 var audio = document.getElementById('audio');
 var playpause = document.getElementById("play");
 
-
 function togglePlayPause() {
    if (audio.paused || audio.ended) {
       playpause.title = "Pause";
@@ -17,4 +16,29 @@ function togglePlayPause() {
       playpause.title = "Play";
       audio.pause();
    }
+}
+
+//traccia iniziale
+//le canzoni che ho messo in music sono 3 i cui file si chiamano come nel tutorial
+//hello.mp3, divine.mp3, beginning.mp3
+audio.src ="http://localhost:3003/music?id=divine.mp3";
+document.getElementById('title').innerHTML="Shape of You";
+document.getElementById('artist').innerHTML="Ed Sheeran";
+
+function changeTracks(){
+        audio.src ="http://localhost:3003/music?id=hello.mp3";
+        audio.load(); 
+        playpause.title = "Pause";
+        audio.play();
+        
+};
+
+function updateSource(id){    
+    audio.src="http://localhost:3003/music?id=" + document.getElementById(id).getAttribute('data-value');
+    //document.getElementById('title').innerHTML="Shape of You";
+    //document.getElementById('artist').innerHTML="Ed Sheeran";
+    audio.load();
+    playpause.title = "Pause"; //non funziona
+    audio.play();
+    
 }
