08a5eca85fc57b502bec1a7598858dc7f21c80fc
Simone Fuoco <simone.fuoco@edu.itspiemonte.it>
2017-11-06 05:29:15 +0100

    Modified GUI + Spotify API finished

--- app.js
+++ app.js
@@ -1,13 +1,143 @@
 var express = require('express');
 var fs = require('fs');
+/*
+var request = require('request');
+
+var cors = require('cors');
+var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
+*/
+/*
+var client_id = ''; // Your client id
+var client_secret = ''; // Your secret
+var redirect_uri = ''; // Your redirect uri
+*/
+
+//var stateKey = 'spotify_auth_state';
 
 var app = express();
-
+/*
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+*/
 app.use('/public', express.static(__dirname + '/public'));
+/*
+app.get('/login', function(req, res) {
 
+  var state = generateRandomString(16);
+  res.cookie(stateKey, state);
 
+  // your application requests authorization
+  var scope = 'user-read-private user-read-email';
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
+});
+*/
+/*
+app.post('/public', function(req, res, next) {
+    var code = req.query.code || null;
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+      }
+    };
+
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+
+        var access_token = body.access_token
+
+        // we can also pass the token to the browser to make requests from there
+        res.redirect('/public' +
+          querystring.stringify({
+            access_token: access_token
+          }));
+      } else {
+        res.redirect('/public' +
+          querystring.stringify({
+            error: 'invalid_token'
+          }));
+      }
+    });
+});
+*/
+/*
+app.get('/callback', function(req, res, next) {
+    var code = req.query.code ? req.query.code : null;
+  var state = req.query.state ? req.query.state : null;
+  var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+  if (!state) {
+      console.log(state);
+    res.redirect('/public?' +
+      querystring.stringify({
+        error: 'state_mismatch'
+    }));
+  } else {
+    res.clearCookie(stateKey);
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+      },
+      json: true
+    };
+
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+
+        var access_token = body.access_token,
+            refresh_token = body.refresh_token;
+
+        var options = {
+          url: 'https://api.spotify.com/v1/me',
+          headers: { 'Authorization': 'Bearer ' + access_token },
+          json: true
+        };
+
+        // use the access token to access the Spotify Web API
+        request.get(options, function(error, response, body) {
+          console.log(body);
+        });
+
+        // we can also pass the token to the browser to make requests from there
+        res.redirect('/public?' +
+          querystring.stringify({
+            access_token: access_token,
+            refresh_token: refresh_token
+        }));
+      } else {
+        res.redirect('/public?' +
+          querystring.stringify({
+            error: 'invalid_token'
+          }));
+      }
+    });
+  }
+});
+*/
 app.get('/', function(req, res) {
-    return res.redirect('./public/home.html');
+    return res.redirect('./public');
 });
 
 
@@ -50,3 +180,20 @@ app.get('/download', function(req, res) {
 app.listen(3003, function() {
     console.log('App listening on port 3003!');
 });
+
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+ /*
+var generateRandomString = function(length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+*/
--- public/main.js
+++ public/main.js
@@ -1,44 +1,104 @@
-// PRESS THE MENU BUTTON TO TRIGGER ANIMATION
-// PRESS PLAY BUTTON TO LISTEN THE DEMO SONG
-
-// As seen on: "https://dribbble.com/shots/2144866-Day-5-Music-Player-Rebound/"
-
-// THANK YOU!
-
 var audio = document.getElementById('audio');
 var playpause = document.getElementById("play");
+var bars = document.getElementById("bars");
+var title = document.getElementById("title");
+var artist = document.getElementById("artist");
 
 function togglePlayPause() {
    if (audio.paused || audio.ended) {
       playpause.title = "Pause";
       audio.play();
+      bars.hidden = false;
    } else {
       playpause.title = "Play";
       audio.pause();
+      bars.hidden = true;
    }
 }
 
 //traccia iniziale
 //le canzoni che ho messo in music sono 3 i cui file si chiamano come nel tutorial
 //hello.mp3, divine.mp3, beginning.mp3
-audio.src ="http://localhost:3003/music?id=divine.mp3";
-document.getElementById('title').innerHTML="Shape of You";
-document.getElementById('artist').innerHTML="Ed Sheeran";
+
+
+audio.src ="http://localhost:3003/music?id=song1.mp3";
+title.innerHTML="song1";
+artist.innerHTML="artist1";
+
 
 function changeTracks(){
         audio.src ="http://localhost:3003/music?id=hello.mp3";
-        audio.load(); 
+        audio.load();
         playpause.title = "Pause";
         audio.play();
-        
+        bars.hidden = false;
 };
 
-function updateSource(id){    
+function updateSource(id){
     audio.src="http://localhost:3003/music?id=" + document.getElementById(id).getAttribute('data-value');
     //document.getElementById('title').innerHTML="Shape of You";
     //document.getElementById('artist').innerHTML="Ed Sheeran";
     audio.load();
     playpause.title = "Pause"; //non funziona
     audio.play();
-    
+
 }
+
+function handleKeyPress(e) { //non usare!
+    console.log("tasto premuto");
+    if(e.KeyCode == 13 || e.which == 13) {
+        console.log("return pressed");
+
+        console.log("call api");
+        playSong('strobe', 'deadmau5');
+    }
+}
+/*
+var urlParams = new URLSearchParams(window.location.search);
+var access_token;
+if (urlParams.has('access_token')) {
+    access_token = urlParams.get('access_token');
+
+    //var search = document.getElementById("searchbar");
+    //var artist_string = search.value.split()[0];
+    //var track_string = search.value.split()[1];
+    playSong('strobe', 'deadmau5');
+}
+*/
+/*
+function searchTracks(query) {
+    $.ajax({
+        url: 'https://api.spotify.com/v1/search',
+        headers: { 'Authorization': 'Bearer ' + access_token },
+        data: {
+            q: query,
+            type: 'track'
+        },
+        success: function (response) {
+
+            if (response.tracks.items.length) {
+                audio.crossOrigin = 'anonymous';
+                var track = response.tracks.items[0];
+                audio.src = track.external_urls.spotify;
+                audio.play();
+                console.log("arrivato");
+                //communicateAction('<div>Playing ' + track.name + ' by ' + track.artists[0].name + '</div><img width="150" src="' + track.album.images[1].url + '">');
+            }
+        }
+    });
+}
+
+function playSong(songName, artistName) {
+    var query = songName;
+    if (artistName) {
+        query += ' artist:' + artistName;
+    }
+
+    searchTracks(query);
+}
+
+function communicateAction(text) {
+    var rec = document.getElementById('conversation');
+    rec.innerHTML += '<div class="action">' + text + '</div>';
+}
+*/
