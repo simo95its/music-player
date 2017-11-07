972cf420e2c156c710f54e21a783adcd3e5c5360
Elena <elena.dinunzio@edu.itspiemonte.it>
2017-11-07 22:36:49 +0100

    modif

--- public/main.js
+++ public/main.js
@@ -5,48 +5,33 @@ var title = document.getElementById("title");
 var artist = document.getElementById("artist");
 
 function togglePlayPause() {
-   if (audio.paused || audio.ended) {
-      $('#btnPlayPause').removeClass('play');
-      $('#btnPlayPause').addClass('pause');
-      audio.play();
-      bars.hidden = false;
-   } else {
-      $('#btnPlayPause').removeClass('pause');
-      $('#btnPlayPause').addClass('play');
-      audio.pause();
-      bars.hidden = true;
-   }
-}
-
-/*
-vecchia funzione
-function listFiles() {
-    var items = {};
-    $.ajax({
-        url: "http://localhost:3003/list"
-    })
-    .done(function (data) {
-        //items = JSON.parse(data);
-        items = data;
-        console.log('data: ' + typeof data);
-    });
-    console.log('items: ' + typeof items)
-    return items;
-} */
-
-function changeTracks(){
-        audio.src ="http://localhost:3003/music?id=hello.mp3";
-        audio.load();
-        playpause.title = "Pause";
+    if (audio.paused || audio.ended) {
+        $('#btnPlayPause').removeClass('play');
+        $('#btnPlayPause').addClass('pause');
+        audio.load()
         audio.play();
         bars.hidden = false;
+    } else {
+        $('#btnPlayPause').removeClass('pause');
+        $('#btnPlayPause').addClass('play');
+        audio.pause();
+        bars.hidden = true;
+    }
 }
 
-function updateSource(id){
-    togglePlayPause();
-    audio.src="http://localhost:3003/music?id=" + document.getElementById(id).getAttribute('id');
+function changeTracks() {
+    audio.src = "http://localhost:3003/music?id=hello.mp3";
     audio.load();
-    togglePlayPause()
+    playpause.title = "Pause";
+    audio.play();
+    bars.hidden = false;
+}
+
+function updateSource(id) {
+    togglePlayPause();
+    audio.src = "http://localhost:3003/music?id=" + document.getElementById(id).getAttribute('id');
+    togglePlayPause();
+
     /*
     var audioPlay = audio.play();
     if (audioPlay !== undefined) {
@@ -58,56 +43,9 @@ function updateSource(id){
     }*/
 }
 
-/* function listSong() {
-    var list = document.getElementById('list');
-    var items = listFiles();
-    for (var i = 0; i < items.length; i++) {
-        var song = document.createElement('tr');
-        song.class = 'song';
-        list.appendChild(song);
-        var nr = document.createElement('td');
-        nr.class = 'nr';
-        var title = document.createElement('td');
-        title.class = 'title';
-        var length = document.createElement('length');
-        length.class = 'length';
-        var heart_td = document.createElement('td');
-        heart_td.id = 'heart_td_' + (i+1);
-        song.appendChild(nr);
-        song.appendChild(title);
-        song.appendChild(length);
-        song.appendChild(heart_td);
-        var number = document.createElement('h5');
-        number.innerHTML = i + 1;
-        nr.appendChild(number);
-        var li_song = document.createElement('li');
-        li_song.id = items[i];
-        li_song.dataValue = items[i];
-        li_song.onclick = updateSource(items[i]);
-        title.appendChild(li_song);
-        var title_header = document.createElement('h6');
-        title_header.innerHTML = items[i];
-        li_song.appendChild(title_header);
-        var no_length = document.createElement('h5');
-        no_length.innerHTML = "length";
-        length.appendChild(no_length);
-        var input = document.createElement('input');
-        input.type = "checkbox";
-        input.id = "heart" + (i+1);
-        input.checked = "true";
-        heart_td.appendChild(input);
-        var label = document.createElement('label');
-        label.class = "zmr";
-        label.for = "heart" + (i+1);
-        label.checked = "true";
-        heart_td.appendChild(label);
-        console.log("L'interfaccia è uscita?");
-    }
-} */
-
 function handleKeyPress(e) { //non usare!
     console.log("tasto premuto");
-    if(e.KeyCode == 13 || e.which == 13) {
+    if (e.KeyCode == 13 || e.which == 13) {
         console.log("return pressed");
 
         console.log("call api");
